0ba0acf2202089fc9ffc1b26c86c73bd1fdae75e
Christian <chrgeneral@gmail.com>
2017-12-16 23:20:51 -0800

    Added Documentation

--- magic8Ball.py
+++ magic8Ball.py
@@ -1,6 +1,9 @@
+# imported module random
 import random
 
+# creating a function 'getanswer'
 def getAnswer(answerNumber):
+    # If , Elif , Else are all used to return values for 'getanswer'
     if answerNumber == 1:
         return 'It is certian'
     elif answerNumber == 2:
@@ -20,6 +23,13 @@ def getAnswer(answerNumber):
     elif answerNumber == 9:
         return 'Very doubtful'
 
+# r gives a random value between 1 - 9 based on the module random
+# int is used to change given value to a integer to be used
 r = random.randint(1, 9)
+
+# Creating variable 'fortune' w/ function 'getAnswer' and 'r'
+# Based on the random value given by r will run in 'getanswer'
 fortune = getAnswer(r)
+
+# Print out the result of variable 'fortune'
 print(fortune)
